3033e3c79493c3bc2b4c0450eb236e8aec67b663
Taylor <UkrainianEagle@q.com>
2019-03-02 01:11:57 -0600

    Updated some files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,22 @@
+let path = require("path");
+
+let friendData = require("../data/friends");
+
+
 module.exports = function (app) {
+
+	// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+	app.get('/api/friends', function(req, res){
+		res.json(friendData);
+	});
+
+	
+
+	// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+		// use req.body?
+
+
+
+
 
 };
